f3499e76b82f047b57594c382750efe91d72ebf7
James Grieve <pocojames@hotmail.com>
2021-04-29 15:40:42 -0700

    import sidenav into the routes

--- app/javascript/routes/App.jsx
+++ app/javascript/routes/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Navbar from "../components/Navbar";
+import SideNav from "../components/SideNav";
 
 const Routes = () => {
   return (
     <Router>
       <Navbar />
+      <SideNav />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
