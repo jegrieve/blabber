338de6166c84a939236f1a37c21bd52faf5ac2b1
James Grieve <pocojames@hotmail.com>
2021-04-29 00:43:58 -0700

    add index, create and destroy actions for the sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::SessionsController < ApplicationController
+    def index
+        if session[:user_id] 
+            user = User.find_by(id: session[:user_id])
+            render json: user
+        end
+    end
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user.present? && user.authenticate(params[:password]) 
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: nil
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        if (!session[:user_id])
+            render json: true
+        else
+            render json: false
+        end
 end
