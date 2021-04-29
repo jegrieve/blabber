4cb7edb2001ed449e6e5b27c6ff6b34a4efca25c
James Grieve <pocojames@hotmail.com>
2021-04-29 00:33:03 -0700

    add create and user_params methods in the registrations controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::RegistrationsController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: user.errors
+        end
+    end
+
+    private
+    def user_params
+        params.require(:registration).permit(:username,:email,:password,:password_confirmation)
+    end
 end
