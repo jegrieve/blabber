c179b04b1792782e552317e332328b28808a7b0a
James Grieve <pocojames@hotmail.com>
2021-04-29 00:45:58 -0700

    add sessions routes for index, create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       post 'registrations/create';
+
+      get 'sessions/index';
+      post 'sessions/create';
+      delete 'sessions/destroy/:id', to: 'sessions#destroy';
+
     end
   end
   root 'homepage#index'
