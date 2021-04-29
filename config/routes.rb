b1655224baf509278a243388334c880bb4bb181a
James Grieve <pocojames@hotmail.com>
2021-04-29 00:38:07 -0700

    add route for registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do 
+    namespace :v1 do
+      post 'registrations/create';
+    end
+  end
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
