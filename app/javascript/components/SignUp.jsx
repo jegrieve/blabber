59a781ba90ddd95ea3b13b3be53e3eca617a43db
James Grieve <pocojames@hotmail.com>
2021-04-30 02:22:10 -0700

    create sign up form component

--- app/javascript/components/SignUp.jsx
+++ app/javascript/components/SignUp.jsx
@@ -4,7 +4,12 @@ import React from "react";
 const SignUp = (props) => {
   return (
       <div>
-          SignUp
+          <form>
+              <input type = "text"></input>
+              <input type = "email"></input>
+              <input></input>
+              <input></input>
+          </form>
       </div>
   )
 }
---
+++ app/javascript/components/SignUpForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const SignUpForm = (props) => {
+  return (
+      <div>
+
+      </div>
+  )
+}
+
+
+export default SignUpForm;
