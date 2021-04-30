cc67bdd477ff005018122e913fc00174a786505d
James Grieve <pocojames@hotmail.com>
2021-04-30 02:12:21 -0700

    add navlinks for signup and signin in the navbar component

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 const Navbar = (props) => {
   return (
       <div className = "site-navbar d-flex justify-content-around align-items-center">
-          <div>User Sign In</div>
-          <div>User Sign up</div>
+          <NavLink to={"/sign-up"}>Sign In</NavLink>
+          <NavLink to = {"/sign-in"}>Sign Up</NavLink>
       </div>
   )
 }
--- app/javascript/routes/App.jsx
+++ app/javascript/routes/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Navbar from "../components/Navbar";
