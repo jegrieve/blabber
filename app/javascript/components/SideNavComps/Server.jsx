7d6c3be818cf3a78726ffb2d135a001c1ec2a8da
James Grieve <pocojames@hotmail.com>
2021-05-04 02:57:22 -0700

    add navlink to go to ServerPage on the server component

--- app/javascript/components/SideNavComps/Server.jsx
+++ app/javascript/components/SideNavComps/Server.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 const Server = (props) => {
+
   return (
       <div>
-          Server
+          <NavLink to = {`/servers/${props.data.id}`}>
+            {props.data.name}
+          </NavLink>
       </div>
   )
 }
