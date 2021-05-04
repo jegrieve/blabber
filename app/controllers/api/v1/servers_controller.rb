4ebdb645c9fc8126434cce0dc74772fd7d5deadd
James Grieve <pocojames@hotmail.com>
2021-05-04 00:26:56 -0700

    add server controller actions

--- app/controllers/api/v1/servers_controller.rb
+++ app/controllers/api/v1/servers_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ServersController < ApplicationController
+    #use serializer later when using image attachment
+    def index
+        @servers = Server.all
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def delete
+    end
 end
