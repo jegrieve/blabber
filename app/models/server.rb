eabbc476d88302aa2a3a154139caec7bf23e6183
James Grieve <pocojames@hotmail.com>
2021-05-03 20:15:06 -0700

    add user association and validations to server model

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,7 @@
 class Server < ApplicationRecord
+    belongs_to :user
+    #has_one_attached :server_image
+    #has_many :channels
+    validates :name, presence: true
+    validates :body, presence: true
 end
