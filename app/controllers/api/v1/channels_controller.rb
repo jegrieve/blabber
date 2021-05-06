25bdb0336d5d2fea2ae1d23ca69502e11553e133
James Grieve <pocojames@hotmail.com>
2021-05-06 01:45:59 -0700

    create show action for channels controller

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::ChannelsController < ApplicationController
+    def show
+        channel = Channel.find(params[:id])
+        if channel
+            render json: channel
+        end
+    end
 end
