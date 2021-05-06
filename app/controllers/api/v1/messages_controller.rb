6380fb0b78b20432a830cb8f5d773f27261cedf1
James Grieve <pocojames@hotmail.com>
2021-05-06 02:50:45 -0700

    create index action in the messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::MessagesController < ApplicationController
+    def index
+        messages = Channel.find(params[:id]).messages.order(created_at: :desc)
+        if messages
+            render json: messages
+        end
+    end
 end
