40e8c933b397c1db5d954dd0fc77f8ef446cba10
James Grieve <pocojames@hotmail.com>
2021-05-06 00:22:57 -0700

    create channel component

---
+++ app/javascript/components/SideNavComps/Channel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Channel = (props) => {
+
+  return (
+      <div>
+          channel
+      </div>
+  )
+}
+
+
+export default Channel;
