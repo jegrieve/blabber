a521e8200fb2ec5197c1cb0de6ce4aa2d21cece9
James Grieve <pocojames@hotmail.com>
2021-05-07 01:06:06 -0700

    add api call method to channelpage component

--- app/javascript/components/ChannelPage.jsx
+++ app/javascript/components/ChannelPage.jsx
@@ -1,8 +1,31 @@
 import React, {useState, useEffect} from "react";
 
 const ChannelPage = (props) => {
-    //messagefeed make the api call
-    //then load all the messages and loop through it in message component
+    const [channelData, setChannelData] = useState(null);
+    console.log(channelData);
+    useEffect(() => {
+        getChannelData();
+    }, [])
+
+    const getChannelData = () => {
+        const id = props.match.params.id
+        const url = `/api/v1/channels/show/${id}`;
+    
+        fetch(url)
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("Network response was not ok.");
+          })
+          .then(response => {
+            setChannelData(response)
+          }
+            )
+          .catch((error) => console.log(error.message));
+      }
+    
+
   return (
       <div>
           ChannelPage
