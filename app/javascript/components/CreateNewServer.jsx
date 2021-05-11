2bc3878d9329f68c4cd15e06f3baad07ea069430
James Grieve <pocojames@hotmail.com>
2021-05-11 00:27:21 -0700

    add form to createnewserver

--- app/javascript/components/CreateNewServer.jsx
+++ app/javascript/components/CreateNewServer.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 
 const CreateNewServer = (props) => {
+  const [createServerInputs, setCreateServerInputs] = useState({
+    serverName: "",
+    serverInfo: ""
+  })
+
+  const enterServerInputs = (e) => {
+    setCreateServerInputs((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value
+    }));
+  };
+
+  const submitCreateServerForm = () => {
+
+  }
 
   return (
       <div>
-        Create a new server
+        <form onSubmit = {submitCreateServerForm}>
+          <label>Server Name:
+            <input name = "serverName" type = "text" onChange = {enterServerInputs} value = {createServerInputs["serverName"]} />
+          </label>
+          <label>Server Info:
+            <input name = "serverInfo" type = "text" onChange = {enterServerInputs} value = {createServerInputs["serverInfo"]} />
+          </label>
+          <button type = "submit">Create</button>
+        </form>
       </div>
   )
 }
