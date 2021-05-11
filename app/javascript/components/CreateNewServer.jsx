6415d7c41d86567e58ebf2d6a0beab7aaa229ecc
James Grieve <pocojames@hotmail.com>
2021-05-11 00:45:55 -0700

    add create action api call to create new servers

--- app/javascript/components/CreateNewServer.jsx
+++ app/javascript/components/CreateNewServer.jsx
@@ -14,9 +14,34 @@ const CreateNewServer = (props) => {
     }));
   };
 
-  const submitCreateServerForm = () => {
-
+  const submitCreateServerForm = (e) => {
+    e.preventDefault();
+      const body = {
+          name: createServerInputs["serverName"],
+          info: createServerInputs["serverInfo"]
+      }
+      const url = "/api/v1/servers/create"
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+      fetch(url, {
+      method: "POST",
+      headers: {
+      "X-CSRF-Token": token, 
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+      .then(response => {
+          if (response.ok) {
+              return response.json()
+          }
+          throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+          console.log(response)
+      })
+      .catch(error => console.log('did not post'))
   }
+  
 
   return (
       <div>
