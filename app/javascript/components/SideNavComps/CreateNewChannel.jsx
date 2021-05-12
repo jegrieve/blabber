1b9b6653c26f4f97c17163fe41000a623f55e9c7
James Grieve <pocojames@hotmail.com>
2021-05-12 16:01:21 -0700

    add input and post function to createnewchannel component

--- app/javascript/components/SideNavComps/CreateNewChannel.jsx
+++ app/javascript/components/SideNavComps/CreateNewChannel.jsx
@@ -1,11 +1,59 @@
 import React from "react";
 
 const CreateNewChannel = (props) => {
-  return (
-      <div>
-          createnewchannel
-      </div>
-  )
-}
+    const [createChannelInputs, setCreateChannelInputs] = useState({
+      channelName: "",
+    })
+  
+    const enterChannelInputs = (e) => {
+      setCreateChannelInputs((prev) => ({
+          ...prev,
+          [e.target.name]: e.target.value
+      }));
+    };
+  
+    const submitCreateChannelForm = (e) => {
+      e.preventDefault();
+      postCreateChannelData();
+  }
+  
+    const postCreateChannelData = () => {
+        const body = {
+            name: createChannelInputs["channelName"],
+        }
+        const url = "/api/v1/channels/create"
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        fetch(url, {
+        method: "POST",
+        headers: {
+        "X-CSRF-Token": token, 
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error("Network response was not ok.");
+        })
+        .then(response => {
+            props.history.push(`/channel/${response.id}`);
+        })
+        .catch(error => console.log('did not post'))
+    }
+    
+  
+    return (
+        <div>
+          <form onSubmit = {submitCreateChannelForm}>
+            <label>Channel Name:
+              <input name = "channelName" type = "text" onChange = {enterChannelInputs} value = {createChannelInputs["channelName"]} />
+            </label>
+            <button type = "submit">Create</button>
+          </form>
+        </div>
+    )
+  }
 
 export default CreateNewChannel;
