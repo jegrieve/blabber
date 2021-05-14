cebc783a8a5ec13d65e42f375d251c1232a37ea3
James Grieve <pocojames@hotmail.com>
2021-05-13 23:28:10 -0700

    add /*path so that when refreshing url, the app won't crash

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
     end
   end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
