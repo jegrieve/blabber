0ed0fb8ae298c4af82a7930c2754bfaa7827de8f
James Grieve <pocojames@hotmail.com>
2021-05-14 22:46:50 -0700

    add created_at method to change the created_at time into a more readable format

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ApplicationRecord
     belongs_to :user
     belongs_to :channel
     validates :body, presence: true
+
+    def created_at
+        attributes['created_at'].strftime("%^b %d %Y at %l:%M%p")
+    end
 end
