365c058780cbe231fd6c16387e46fef97fea2c8f
James Grieve <pocojames@hotmail.com>
2021-05-14 23:34:24 -0700

    add fontawesome library to react

--- app/javascript/components/Message.jsx
+++ app/javascript/components/Message.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Message = (props) => {
   return (
     <div>
+      <div className = "message-avatar">
+        {/* {props.messageData.user.username} 
+        eventually use link to img*/}
+      </div>
+      <div className = "message-username">
+        {props.messageData.user.username}
+      </div>
+      <div className = "message-body">
         {props.messageData.body}
+      </div>
+      <div className = "message-time">
+        {props.messageData.created_at}
+      </div>
     </div>
   )
 }
