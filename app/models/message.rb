c914ae232615687b72b8073ca6ddf42ad4bb1863
James Grieve <pocojames@hotmail.com>
2021-05-16 17:10:13 -0700

    add message_image to the message serializer, allowing for images to be attached to the message response

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
     belongs_to :user
     belongs_to :channel
     validates :body, presence: true
+    has_one_attached :message_image
 
     def created_at
         attributes['created_at'].strftime("%b %d %Y at %l:%M%p")
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,11 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :user, :created_at
+  include Rails.application.routes.url_helpers
+  attributes :id, :body, :user, :created_at, :message_image
+  def message_image
+    if object.message_image.attached?
+      {
+        url: rails_blob_url(object.message_image)
+      }
+    end
+  end
 end
