0af19ad18085bb88fba6c350ddbe126d1897fef5
James Grieve <pocojames@hotmail.com>
2021-05-28 16:51:30 -0700

    add index action to channels controller

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::ChannelsController < ApplicationController
+    def index
+        channels = Server.find(params[:server_id]).channels.offset(params[:offset_num]).limit(2);
+        # servers = Server.all.offset(params[:offset_num]).limit(15)
+        # servers = Server.all
+        render json: channels
+    end
+
     def show
         channel = Channel.find(params[:id])
         if channel
