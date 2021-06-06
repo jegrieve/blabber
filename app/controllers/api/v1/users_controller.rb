ae6467aafceca7f056eadc57c4e438ebb9362b2f
James Grieve <pocojames@hotmail.com>
2021-06-05 20:14:42 -0700

    create user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        if user
+            render json: user
+        end
+    end
+
+    def destroy
+    end
+
+    def update
+    end
 end
