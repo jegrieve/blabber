b9390a224fdf497d2f1fcff8d1f15cf6cd66a91c
James Grieve <pocojames@hotmail.com>
2021-06-13 01:37:41 -0700

    add basic layout of userpage

--- app/javascript/components/UserPage.jsx
+++ app/javascript/components/UserPage.jsx
@@ -2,7 +2,10 @@ import React, {useEffect, useState} from "react";
 
 const UserPage = (props) => {
     const [userData, setUserData] = useState(null);
-
+  // ability to set bio/avatar
+  //ability to see a bio/avatar of other users
+  //a component to see some recent activity
+  //also ability to delete account
     useEffect(() => {
         const id = props.match.params.id
         const url = `/api/v1/users/show/${id}`;
@@ -23,8 +26,21 @@ const UserPage = (props) => {
 
   return (
       <div className = "page-display userpage">
-          Userpage
-          {userData ? <div>{userData.username}</div> : false}
+          {userData ? 
+            <div>
+              <div>
+                {userData.username}
+              </div>
+              <div>
+                FontAwesomeUserIconPlaceholder
+              </div>
+              <div>
+                Bio goes here
+              </div>
+              <div>
+                RecentActivity
+              </div>
+            </div> : <div>Oops, something went wrong. User does not exist.</div>}
       </div>
   )
 }
