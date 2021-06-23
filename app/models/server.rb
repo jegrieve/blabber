8264959ea2379354e391694bcd158af3b68ab597
James Grieve <pocojames@hotmail.com>
2021-06-22 20:08:03 -0700

    add server_image to server serializer and model

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,7 +1,8 @@
 class Server < ApplicationRecord
     belongs_to :user
     has_many :channels
-    #has_one_attached :server_image
+    has_one_attached :server_image
+    
     validates :name, presence: true, uniqueness: true
     validates :info, presence: true
 end
--- app/serializers/server_serializer.rb
+++ app/serializers/server_serializer.rb
@@ -1,3 +1,11 @@
 class ServerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :info, :channels
+  attributes :id, :name, :info, :channels, :server_image
+
+  def server_image
+    if object.server_image.attached?
+      {
+        url: rails_blob_url(object.server_image)
+      }
+    end
+  end
 end
