74987ed841d6428ef2451b7ae7e505c9b3ca525e
James Grieve <pocojames@hotmail.com>
2021-06-24 00:48:52 -0700

    add edit functionality for the server image and server info

--- app/javascript/components/EditServerData.jsx
+++ app/javascript/components/EditServerData.jsx
@@ -1,12 +1,120 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
 const EditServerData = (props) => {
-  return (
-      <div>
-Edit
-      </div>
-  )
+    const [imageData, setImageData] = useState({
+        image: null
+      });
+      const [editImage, setEditImage] = useState(false);
+      const [infoText, setInfoText] = useState("");
+      const [editInfo, setEditInfo] = useState(false);
+    
+      useEffect(() => {
+        if (!imageData.image) {
+          setEditImage(false);
+        }
+      }, [imageData])
+    
+      useEffect(() => {
+        if (!props.serverData.info) {
+          setInfoText("This server has no info.")
+        } else {
+          setInfoText(props.serverData.info)
+        }
+      },[editInfo])
+    
+      useEffect(() => {
+        if (editInfo === "submitted") {
+          props.updateServerInfo(props.serverData.id, infoText);
+        }
+        if (editImage === "submitted") {
+          props.updateServerImage(props.serverData.id, imageData);
+        }
+      }, [editInfo, editImage])
+    
+      const handleImage = () => {
+        if (!editImage || editImage === "submitted") {
+          setEditImage(true)
+        } else {
+          setEditImage(false)
+        }
+      }
+    
+      const handleInfo = () => {
+        if (!editInfo || editInfo === "submitted") {
+          setEditInfo(true)
+        } else {
+          setEditInfo(false)
+        }
+      }
+    
+      const onImageChange = (e) => {
+        setImageData((prev) => ({
+            image: e.target.files[0]
+        }))
+      };
+    
+      const saveEditImage = () => {
+        setEditImage("submitted")
+      }
+    
+      const exitEditImage = () => {
+        setImageData({
+          image: null
+        })
+      }
+    
+      const saveEditInfo = () => {
+        setEditInfo("submitted")
+      }
+    
+      const exitEditInfo = () => {
+        setEditInfo(!editInfo)
+      }
+    
+      const onInfoInputChange = (e) => {
+        setInfoText(e.target.value);
+      }
+    
+      return (
+          <div>
+              <div>{props.serverData.name}</div>
+              <div>
+                  {props.serverData.server_image ? 
+                  <div>
+                      <img src = {props.serverData.server_image.url} width = {300} />
+                  </div> 
+                  :
+                //   <FontAwesomeIcon icon = {faUserCircle} />
+                <div>placeholder image</div>
+                  }
+                   {editImage === true ? 
+                     <div>
+                      <div className = "form-group" >
+                        <input className = "form-control" name = "image" type="file" accept="image/*" multiple={false} onChange={onImageChange} /> 
+                      </div>
+                      <button onClick = {saveEditImage}>Save</button>
+                      <button onClick = {exitEditImage}>Cancel</button>
+                     </div> : 
+                     <div>
+                       <button onClick = {handleImage}>Edit Image</button>
+                      </div>}
+              </div>
+              <div>
+                {editInfo === true ? 
+                <div>
+                  <textarea value = {infoText} onChange = {onInfoInputChange} />
+                    <button onClick = {saveEditInfo}>Save</button>
+                    <button onClick = {exitEditInfo}>Cancel</button>
+                </div> 
+                : 
+                <div>
+                    {!props.serverData.info ? <div>This server has no info.</div> : <div>{props.serverData.info}</div>}
+                    <button onClick = {handleInfo}>Edit info</button>
+                </div>}
+              </div>
+          </div>
+      )
 }
 
 
--- app/javascript/components/ServerPage.jsx
+++ app/javascript/components/ServerPage.jsx
@@ -3,44 +3,98 @@ import ShowServerData from "./ShowServerData";
 import EditServerData from "./EditServerData";
 
 const ServerPage = (props) => {
-  const [serverData, setServerData] = useState(null)
+  const [serverData, setServerData] = useState(null);
+  const [serverPageId, setServerPageId] = useState(null);
 
   useEffect(() => {
-      getServerData();
-  }, [])
+    getServerData();
+  },[])
 
   useEffect(() => {
-    if (serverData) {
-      props.setCurrentServer(serverData);
+    if (props.match.params.id !== serverPageId) {
+      setServerPageId(props.match.params.id);
     }
-  }, [serverData])
+  })
+  
+  useEffect(() => {
+    getServerData();
+  }, [serverPageId])
 
-  const getServerData = () => {
-    const id = props.match.params.id
-    const url = `/api/v1/servers/show/${id}`;
+    const getServerData = () => {
+      const id = props.match.params.id
+      const url = `/api/v1/servers/show/${id}`;
+  
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .then(response => {
+          console.log(response)
+          setServerData(response)
+      })
+        .catch(() => console.log("error"));
+    }
 
-    fetch(url)
+    const updateServerImage = (serverId, imageData) => {
+      const formData =  new FormData();
+      formData.append('server_image', imageData["image"]);
+      const url = `/api/v1/servers/update/${serverId}`;
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+      fetch(url, {
+      method: "PATCH",
+      body: formData,
+      headers: {
+      "X-CSRF-Token": token, 
+    },
+  })
       .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Network response was not ok.");
+          if (response.ok) {
+              return response.json()
+          }
+          throw new Error("Network response was not ok.");
       })
       .then(response => {
-        setServerData(response)
-      }
-        )
-      .catch((error) => console.log(error.message));
-  }
-  // components updateServerImage = {updateServerImage} updateServerInfo = {updateServerInfo}
+        getServerData();
+      })
+      .catch(error => console.log(error.message))
+    }
+
+    const updateServerInfo = (serverId, infoText) => {
+      const body = {
+        info: infoText,
+    }
+      const url = `/api/v1/servers/update/${serverId}`;
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+      fetch(url, {
+      method: "PATCH",
+      headers: {
+      "X-CSRF-Token": token, 
+      "Content-Type": "application/json"
+    },
+      body: JSON.stringify(body)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw new Error("Network response was not ok.");
+    })
+    .then(response => {
+        getServerData();
+    })
+    .catch(error => console.log(error.message))
+    }
 
   return (
-    <div className = "page-display userpage">
-    {props.currentUser 
-    && serverData 
-    && props.currentUser.id === serverData.user.id
+    <div className = "page-display serverpage">
+    {props.currentUser && 
+    serverData && 
+    props.currentUser.id === serverData.user.id
     ? 
-    <EditServerData serverData = {serverData}  />
+    <EditServerData serverData = {serverData} updateServerImage = {updateServerImage} updateServerInfo = {updateServerInfo} />
     : serverData ? 
     <ShowServerData serverData = {serverData} />
     :
