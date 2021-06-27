1ad0c01f4d690b4805574ac9931a867133500ed1
James Grieve <pocojames@hotmail.com>
2021-06-27 01:14:00 -0700

    add dependent destroy to the server/channel/messages  associations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,6 @@
 class Channel < ApplicationRecord
     belongs_to :server
-    has_many :messages
+    has_many :messages, dependent: :destroy
 
     validates :name, presence: true, uniqueness: true
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,6 +1,6 @@
 class Server < ApplicationRecord
     belongs_to :user
-    has_many :channels
+    has_many :channels, dependent: :destroy
     has_one_attached :server_image
     
     validates :name, presence: true, uniqueness: true
