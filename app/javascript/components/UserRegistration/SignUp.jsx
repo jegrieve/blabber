cd3a5bbe71473390ae26b7b699a03419226c064d
James Grieve <pocojames@hotmail.com>
2021-06-30 02:38:40 -0700

    create page title for user reg

--- app/javascript/components/UserRegistration/SignUp.jsx
+++ app/javascript/components/UserRegistration/SignUp.jsx
@@ -3,8 +3,11 @@ import SignUpForm from "./SignUpForm";
 
 const SignUp = (props) => {
   return (
-      <div className = "page-display sign-up-container d-flex justify-content-center">
-        <SignUpForm history = {props.history} setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
+      <div className = "page-display sign-up-container">
+        <div className = "page-title">Sign Up</div>
+        <div className = "sign-up-container d-flex justify-content-center">
+          <SignUpForm history = {props.history} setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
+        </div>
       </div>
   )
 }
