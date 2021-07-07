a0b59bf6b5d5989c612d0359a0cff0b02f8944d2
James Grieve <pocojames@hotmail.com>
2021-07-07 16:42:20 -0700

    add pages_info controller show action, api request on homepage

--- app/controllers/api/v1/pages_info_controller.rb
+++ app/controllers/api/v1/pages_info_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::PagesInfoController < ApplicationController
+    def show #add route, so start with one action at atime, make sure to add both the params showing what we want Aswell as the params with id/etc
+        if params[:homepage]
+            servers = Server.all.length
+            users = User.all.length
+            render json: {servers: servers, users: users}
+        end
+    end
 end
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,11 +1,33 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { NavLink } from "react-router-dom";
 
 const Home = (props) => {
+  const [homepageInfo, setHomepageInfo] = useState(null);
   useEffect(() => {
     props.setCurrentServer(null);
   },[])
   
+  useEffect(() => {
+    getHomeData();
+  },[])
+
+  const getHomeData = () => {
+    const id = props.match.params.id
+    const url = `/api/v1/pages/show?homepage=${true}`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        setHomepageInfo(response);
+    })
+      .catch(() => console.log("error"));
+  }
+
   return (
     <div className = "page-display page-centered">
         <div className = "page-title">Welcome to MessageApp!</div>
@@ -26,6 +48,12 @@ const Home = (props) => {
               to begin chatting
             </span>
           </div>}       
+          {homepageInfo ? 
+          <div>
+            <div>{homepageInfo.servers} servers</div>
+            <div>{homepageInfo.users} users</div>
+          </div>
+        :false}
         </div>
 
     </div>
