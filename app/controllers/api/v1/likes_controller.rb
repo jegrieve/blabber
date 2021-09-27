be4996e96f96dc76a07d03e3f3b8e3fea23df97d
James Grieve <pocojames@hotmail.com>
2021-09-27 02:20:10 -0700

    add create and destroy actions for likes controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::LikesController < ApplicationController
+    def create
+        user = User.find_by(id: session[:user_id])
+        like = user.likes.new
+        like.server_id = params[:server_id]
+        like.save
+        render json: like
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        server = user.likes.find_by(server_id: params[:id])
+        server.destroy
+        render json: {message: 'Unfavourited this server'}
+    end
 end
