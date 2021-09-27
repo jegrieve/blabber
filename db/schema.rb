32a04ac1e830b518fa8554aa5bc25373804c4a88
James Grieve <pocojames@hotmail.com>
2021-09-27 01:16:53 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_064242) do
+ActiveRecord::Schema.define(version: 2021_09_27_081601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2021_06_28_064242) do
     t.index ["server_id"], name: "index_channels_on_server_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "server_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["server_id"], name: "index_likes_on_server_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body", null: false
     t.bigint "user_id", null: false
@@ -85,6 +94,8 @@ ActiveRecord::Schema.define(version: 2021_06_28_064242) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "channels", "servers"
+  add_foreign_key "likes", "servers"
+  add_foreign_key "likes", "users"
   add_foreign_key "messages", "channels"
   add_foreign_key "messages", "users"
   add_foreign_key "servers", "users"
