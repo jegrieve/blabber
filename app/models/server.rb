e81b0397e8bbe5f45b25544763cd9643d3c87afa
James Grieve <pocojames@hotmail.com>
2021-09-27 01:19:24 -0700

    add associations for likes and liking_users in server model

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,6 +1,8 @@
 class Server < ApplicationRecord
     belongs_to :user
     has_many :channels, dependent: :destroy
+    has_many :likes, dependent: :destroy
+    has_many :liking_users, :through => :likes, :source => :user, dependent: :destroy
     has_one_attached :server_image
     
     validates :name, presence: true, uniqueness: true
