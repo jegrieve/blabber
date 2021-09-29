7bd6e695749e91beb4319e80cb68652a7d82892b
James Grieve <pocojames@hotmail.com>
2021-09-28 20:38:44 -0700

    add getChannelData function to EditChannel

--- app/javascript/components/SideNavComps/EditChannel.jsx
+++ app/javascript/components/SideNavComps/EditChannel.jsx
@@ -1,6 +1,35 @@
 import React, { useEffect } from "react";
 
 const EditChannel = (props) => {
+  const [editChannelData, setEditChannelData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+
+  useEffect(() => {
+    getChannelData();
+  },[]);
+
+  useEffect(() => {
+    setEditChannelData({...channelData})
+  }, [channelData])
+
+  const getChannelData = () => {
+    const id = props.match.params.id
+    const url = `/api/v1/channels/show/${id}`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        console.log(response)
+        setChannelData(response)
+    })
+      .catch(() => console.log("error"));
+  }
+
 
   return (
       <div>
