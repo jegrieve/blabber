109d3b83dec76c2cd8bfd5a46125e219233edfdb
James Grieve <pocojames@hotmail.com>
2021-09-28 20:25:25 -0700

    create EditChannel.jsx

---
+++ app/javascript/components/EditChannel.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const EditChannel = (props) => {
+
+  return (
+      <div>
+          
+      </div>
+  )
+}
+
+export default EditChannel;
--- app/javascript/components/Home/Home.jsx
+++ app/javascript/components/Home/Home.jsx
@@ -138,3 +138,5 @@ const Home = (props) => {
 
 
 export default Home;
+
+
